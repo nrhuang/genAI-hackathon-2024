47f452d962660a48263876d119258eb8e61f4d37
nrhuang <nhuangra@gmail.com>
2024-03-02 14:24:34 -0800

    added image carousel

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import boto3
 import json
 import bedrock
 
+from streamlit_carousel import carousel
+
 st.header("BrainChip")
 st.subheader("Learn Something new today!")
 
@@ -14,7 +16,7 @@ topic = form.text_input(label='Topic')
 level = form.selectbox('Select a Level', ['Beginner', 'Intermediate', 'Advanced'])
 time = form.text_input(label='Time (hours)')
 submit_button = form.form_submit_button(label='Submit')
-invoke_string_notes = "I want to learn about" + topic + "and I have" + time + "hours. I am looking for " + level + "information. Give me short notes about it."
+invoke_string = "I want to learn about" + topic + "and I have" + time + "hours. I am looking for " + level + "information. Give me short notes about it."
 
 if submit_button:
     col1, col2 = st.columns([2, 3.5])
@@ -22,7 +24,7 @@ if submit_button:
 
     with col1:
         st.subheader("Notes")
-        notes = bedrock.invoke(invoke_string_notes, 0.5, 2048)
+        notes = bedrock.invoke(invoke_string, 0.5, 2048)
         st.write(notes)
         
         st.subheader("Suggestions")
@@ -30,10 +32,28 @@ if submit_button:
 
     with col2:
         st.subheader("Slides")
-
         
         st.subheader("Chatbot")
 
+test_items = [
+    dict(
+        title="Slide 1",
+        text="A tree in the savannah",
+        interval=None,
+        img="https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg?w=1380&t=st=1688825493~exp=1688826093~hmac=cb486d2646b48acbd5a49a32b02bda8330ad7f8a0d53880ce2da471a45ad08a4",
+    ),
+    dict(
+        title="Slide 2",
+        text="A wooden bridge in a forest in Autumn",
+        img="https://img.freepik.com/free-photo/beautiful-wooden-pathway-going-breathtaking-colorful-trees-forest_181624-5840.jpg?w=1380&t=st=1688825780~exp=1688826380~hmac=dbaa75d8743e501f20f0e820fa77f9e377ec5d558d06635bd3f1f08443bdb2c1",
+    ),
+    dict(
+        title="Slide 3",
+        text="A distant mountain chain preceded by a sea",
+        img="https://img.freepik.com/free-photo/aerial-beautiful-shot-seashore-with-hills-background-sunset_181624-24143.jpg?w=1380&t=st=1688825798~exp=1688826398~hmac=f623f88d5ece83600dac7e6af29a0230d06619f7305745db387481a4bb5874a0",
+    ),
+]
 
+carousel(items=test_items, width=1)
     
 
